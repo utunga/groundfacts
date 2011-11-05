c7a61535343870c80048a9ee6b69d171aaa3f928
Simon Coggins <simoncoggins@gmail.com>
2011-11-06 12:52:23 +1300

    Refactoring display code

--- backbone/twopage.js
+++ backbone/twopage.js
@@ -43,12 +43,14 @@ $(function() {
         initialize: function() {
             this.renderAll();
             this.renderFiltered();
+            /*
             window.Tweets.bind('add', function(item) {
                 window.AppView.renderAll();
             });
             window.FilteredTweets.bind('add', function(item) {
                 window.AppView.renderFiltered();
             });
+            */
         },
 
         /*
@@ -61,6 +63,43 @@ $(function() {
             // test tweet
             var newTweet = new window.Tweet({timestamp: 9999, message: 'new tweet', user: 'new user'});
             window.Tweets.add(newTweet);
+        },
+        json2model: function(item) {
+            var itemDate = item['value']['created_at'];
+            var itemUser = item['value']['screen_name'];
+            var itemProfileURL = item['value']['profile_image_url'];
+            var itemLat = item['value']['lat'];
+            var itemLon = item['value']['lon'];
+            var itemMessage = item['value']['text'];
+            var myTweet = new window.Tweet({
+                timestamp: itemDate,
+                user: itemUser,
+                profile_url: itemProfileURL,
+                lat: itemLat,
+                lon: itemLon,
+                message: TwitterText.auto_link(itemMessage)
+            });
+            return myTweet;
+        },
+        addAll: function(data) {
+            var rows = data.rows;
+            window.Tweets.reset();
+            $.each(rows, function(index, item) {
+                var myTweet = window.AppView.json2model(item);
+                window.Tweets.add(myTweet);
+                
+            });
+            window.AppView.renderAll();
+        },
+        addFiltered: function(data) {
+            var rows = data.rows;
+            window.FilteredTweets.reset();
+            $.each(rows, function(index, item) {
+                var myTweet = window.AppView.json2model(item);
+                window.FilteredTweets.add(myTweet);
+                
+            });
+            window.AppView.renderFiltered();
         },
 
         renderAll: function() {
@@ -85,28 +124,16 @@ $(function() {
         
     });
 
+    // get all data
     window.AppView = new AppView;
+
     $.get('https://cloudant.com/db/occutweet/occ/_design/app/_view/tweetfall', {limit:50,descending:'true',reduce:'false'}, function(data) {
-        var rows = data.rows;
-        $.each(rows, function(index, item) {
-            var itemDate = item['value']['created_at'];
-            var itemUser = item['value']['screen_name'];
-            var itemProfileURL = item['value']['profile_image_url'];
-            var itemLat = item['value']['lat'];
-            var itemLon = item['value']['lon'];
-            var itemMessage = item['value']['text'];
-            var myTweet = new window.Tweet({
-                timestamp: itemDate,
-                user: itemUser,
-                profile_url: itemProfileURL,
-                lat: itemLat,
-                lon: itemLon,
-                message: TwitterText.auto_link(itemMessage)
-            });
-            window.Tweets.add(myTweet);
-            window.FilteredTweets.add(myTweet);
-            
-        });
+        window.AppView.addAll(data);
+    }, 'jsonp');
+
+    // get filtered data
+    $.get('https://cloudant.com/db/occutweet/occ/_design/app/_view/tweetfall', {limit:50,descending:'true',reduce:'false'}, function(data) {
+        window.AppView.addFiltered(data);
     }, 'jsonp');
 });
 
