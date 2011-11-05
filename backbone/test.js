c78193e91a60f1f7beff851509880d3ef68a9fb2
Simon Coggins <simoncoggins@gmail.com>
2011-11-05 19:39:27 +1300

    Add backbone test directory

---
+++ backbone/test.js
@@ -0,0 +1,60 @@
+
+$(function() {
+
+    // an individual tweet
+    window.Tweet = Backbone.Model.extend({
+        defaults: {
+                      timestamp: false,
+                      message: ''
+                  }
+    });
+
+    // a set of tweets
+    window.Tweetset = Backbone.Collection.extend({
+        model: Tweet,
+        initialize: function() {
+            //console.log('starting collection');
+        }
+    });
+ 
+    // global collection of tweets
+    window.Tweets = new Tweetset;
+
+    // manually add to the collection
+    window.Tweets.add({timestamp: 1234, message: 'this is a tweet'});
+    // another way
+    var myTweet = new window.Tweet // new tweet using defaults
+    window.Tweets.add(myTweet);
+
+    window.TweetView = Backbone.View.extend({
+        tagName: "div",
+        template: _.template($('#tweet-template').html()),
+
+        render: function() {
+            $(this.el).html(this.template(this.model.toJSON()));
+            return this;
+        },
+    });
+
+    window.AppView = Backbone.View.extend({
+        el: $('#mydiv'),
+
+        template: _.template($('#tweet-template').html()),
+
+        initialize: function() {
+            //window.Tweets.fetch();
+            this.render();
+        },
+
+        render: function() {
+            window.Tweets.map(function(tweet) {
+                var view = new TweetView({model: tweet});
+                $(this.el).append(view.render().el);
+            });
+            return this;
+        },
+        
+    });
+
+    window.AppView = new AppView;
+});
